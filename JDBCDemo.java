db766bf155f53f00888e52f263891eeb18b38c3c
shilpiprojects <shilpitejeswar@gmail.com>
2025-07-15 22:14:11 +0530

    Update JDBCDemo.java

--- JDBCDemo.java
+++ JDBCDemo.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class JDBCDemo {
-    public static void main(String[] args) throws Exception {
+    public staticc void main(String[] args) throws Exception {
 
         Class.forName("com.mysql.cj.jdbc.Driver");
 
